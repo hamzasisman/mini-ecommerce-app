c85eabb6c5d031d5cbfb6a78cdecdcb899d6990d
hamzasisman <hamza.sismann@gmail.com>
2023-07-24 17:43:08 +0300

    initialize the project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'quicksand': ['Quicksand']
+      },
+      colors: {
+        'base-text': '#333333',
+      },
+    },
+  },
+  plugins: [],
+}
+
